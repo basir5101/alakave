ec1d410f83b0f25e3d2c211c937bf5b115e54945
basir5101 <basir.bsmrstu@gmail.com>
2024-06-21 17:51:42 +0600

    login page fixed

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,17 +1,17 @@
 import { redirect } from "next/navigation";
 
-import { isUserAuthenticated } from "@/lib/firebase/firebase-admin";
+// import { isUserAuthenticated } from "@/lib/firebase/firebase-admin";
 import PageContent from "@/components/PageContent";
 import Navbar2 from "@/components/common/Navbar2";
-import { useLocale } from "next-intl";
+// import { useLocale } from "next-intl";
+// import { Suspense } from "react";
 
 export default async function SignInPage() {
-  const locale = useLocale();
-  if (await isUserAuthenticated()) redirect(`/${locale}/home_screen`);
+  // const locale = useLocale();
+  // if (await isUserAuthenticated()) redirect(`/${locale}/home_screen`);
 
   return (
     <div>
-      {/* <Navbar2 /> */}
       <PageContent variant="sign-in" />
     </div>
   );
